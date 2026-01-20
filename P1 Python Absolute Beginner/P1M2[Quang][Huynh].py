b63dc39bfa04a0f09e3a5c521bcbf5d9119d06bf
Quang Hai Huynh <qhuynh6@students.kennesaw.edu>
2026-01-20 04:12:05 -0500

    P1M2

---
+++ P1 Python Absolute Beginner/P1M2[Quang][Huynh].py
@@ -0,0 +1,9 @@
+# [ ] create, call and test fishstore() function 
+name = "Quang Huynh"
+def fish_store(fish,price):
+    statement = 'Hi, ' + name + ', ' + fish + ' costs $' + price + '!'
+    return statement
+fish_entry = input('Enter the name of fish: ')
+price_entry = input('Enter the price of fish: ')
+final = fish_store(fish_entry,price_entry)
+print(final) 
