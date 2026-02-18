d7b73d6e40991fc3a72e9370eec2621a72c17f4d
Quang Hai Huynh <qhuynh6@students.kennesaw.edu>
2026-02-18 18:05:50 -0500

    more learning of def join, string module

--- 1 gradetracker/autoguess_password.py
+++ 1 gradetracker/autoguess_password.py
@@ -32,7 +32,7 @@ characters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q
 guess = [''] * len(pass_word)
 try_total = 0
 
-while ''.join(guess) != pass_word:
+while ''.join(guess) != pass_word: # join function combines elements of a list into a string
     for i in range(len(pass_word)):
         if guess[i] != pass_word[i]:
             guess[i] = random.choice(characters)
@@ -41,4 +41,30 @@ while ''.join(guess) != pass_word:
     print(f"try #{try_total}: {''.join(guess)}")
 
 print(f"\nPassword cracked in {try_total} tries!")
+
+'''
+Version With Uppercase + Numbers
+'''
+'''
+Version With Uppercase + Numbers
+'''
+import random
+import string #string module contains collection of string constants
+
+pass_word = 'chico'
+character = string.ascii_letters + string.digits
+guess = [''] * len(pass_word)
+try_total = 0
+
+while ''.join(guess) != pass_word:
+    for i in range(len(pass_word)):
+        if guess[i] != pass_word[i]:
+            guess[i] = random.choice(character)
+    try_total += 1
+    print(f"try # {try_total}: {''.join(guess)}")
+
+print(f"guess successfully at try#{try_total}: {''.join(guess)}")
+
+
+
 
