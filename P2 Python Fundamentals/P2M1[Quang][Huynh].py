7b7c18d77b8f479bc7bc78c08cbc437cdf364f17
Quang Hai Huynh <qhuynh6@students.kennesaw.edu>
2026-03-23 15:29:07 -0400

    fixing p2m1: code put in function

--- P2 Python Fundamentals/P2M1[Quang][Huynh].py
+++ P2 Python Fundamentals/P2M1[Quang][Huynh].py
@@ -1,16 +1,25 @@
-quote = input('Welcome, Quang Huynh. Please enter one sentence quote, alpha seperate words: ')
-word = ''
+def string_filter(quote):
+    word = ''
+    for character in quote:
+        if character.isalpha():
+            word += character
+        else:
+            if word != '': #handle case of string argument starting or/and ending with non-alpha characters 
+                if word[0].lower() > 'g':
+                    print(word.upper())
+                word = ''
+    if word != '': #handle case of string argument ending with alphabetical character
+        if word[0].lower() > 'g':
+            print(word.upper())
+    
+user_input = input('Welcome, Quang Huynh. Please enter one sentence quote, alpha seperate words: ')
+string_filter(user_input)
+   
 
-for character in quote:
-    if character.isalpha():
-        word += character
-    else:
-        if word != '':
-            if word[0].lower() >= 'h':
-                print(word.upper(),)
-                word = ''
-            else:
-                word = ''
-if word != '':
-    if word[0].lower() >= 'h':
-        print(word.upper())
+    
+
+    
+    
+
+
+
